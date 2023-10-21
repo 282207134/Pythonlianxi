a278f6f6d9bb5b09cf5d989adf2de2e5eb7a66bb
liuyu <liu282207134@yahoo.co.jp>
2023-10-21 11:33:04 +0900

    练习5

--- N以内的素数.py
+++ N以内的素数.py
@@ -1,8 +1,9 @@
+N=int(input())
 i = 2
-while(i < 100):
+while(i < N):
    j = 2
    while(j <= (i/j)):
       if not(i%j): break
       j = j + 1
-   if (j > i/j) : print(i, " 是素数")
+   if (j > i/j) : print(i)
    i = i + 1
